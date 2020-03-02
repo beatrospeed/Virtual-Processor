62807a65eb3a96a198254fe0a543d3b228d97d59
beatrospeed <44065356+beatrospeed@users.noreply.github.com>
2020-03-02 18:03:48 -0500

    General purpose registers added

--- Datapath.v
+++ Datapath.v
@@ -17,7 +17,9 @@ module dataPath(
 	 BusMuxin_MDRout, BusMuxin_In_Portout, BusMuxin_Cin,
 	 BusMuxin_in24, BusMuxin_in25, BusMuxin_in26,
 	 BusMuxin_in27, BusMuxin_in28, BusMuxin_in29,
-	 BusMuxin_in30, BusMuxin_in31, Y_in, Z_in,
+	 BusMuxin_in30, BusMuxin_in31, Y_in, Z_in, 
+
+	 input [31:0] R0Val,R1Val,R2Val,R3Val,R4Val,R5Val,R6Val,R7Val,R8Val,R9Val,R10Val,R11Val,R12Val,R13Val,R14Val,R15Val,
 
 	 input load, //load enable for mdr
 	 input clk,
@@ -27,25 +29,43 @@ module dataPath(
 	 input ADD, SUB, MUL, DIV, SHR, SHL, ROR, ROL, AND, OR, NEG, NOT, IncPc,
 	 output [31:0] Mdatain,
 	 output [31:0] ZVal, 
-	 output [31:0] Bus, IRval, MDRval,HIval, LOval
+	 output [31:0] bus, IRval, MDRval,HIval, LOval
 	 
-
 );
+	
 	wire [4:0] Select_D; 
 	wire [31:0] Y_D, InPort_D, OutPort_D, MAR_D, PCVal_D; 
 	wire [63:0] ALUVal_D; 
 	
 	
-	Reg32 Y(Bus, clk, reset, y_in, Y_D); 
+	Reg32 Y(bus, clk, reset, Y_in, Y_D); 
 	Reg32 Z(ALUVal_D, clk, reset, Z_in, Zval);
-	Reg32 HI(Bus, clk reset, HIin, HIval); 
-   Reg32 LO(Bus, Clock, Reset, LOin, LOval);
-	Reg32 InPort(32'b0, Clock, Reset, InPortin, InPortVal);
-   Reg32 OutPort(bus, Clock, Reset, OutPortin, OutPortVal);
+	Reg32 HI(Bus, clk, reset, HIin, HIval); 
+   Reg32 LO(Bus, Clock, reset, LOin, LOval);
+	Reg32 InPort(32'b0, clk, reset, InPortin, InPortVal);
+   Reg32 OutPort(bus, clk, reset, OutPortin, OutPortVal);
    Reg32 MDR(bus, clk, reset, MARin, MAR_D);
-   mdr_reg mdr(bus ,MDRval, mDataIn, Read,Reset, Clock, MDRin);
-   Reg32 IR(bus, Clock, Reset, IRin, IRVal);
-   Reg32 PC(bus, Clock, Reset, PCin, PCVal);
+   mdr_reg mdr(bus ,MDRval, mDataIn, load ,reset, clock, MDRin);
+   Reg32 IR(bus, clk, reset, IRin, IRval);
+   Reg32 PC(bus, clk, reset, PCin, PCVal);
+   Reg32 R0(bus, clk, reset, R0in, R0Val); 
+   Reg32 R1(bus, clk, reset, R1in, R1Val); 
+   Reg32 R2(bus, clk, reset, R2in, R2Val); 
+   Reg32 R3(bus, clk, reset, R3in, R3Val); 
+   Reg32 R4(bus, clk, reset, R4in, R4Val); 
+   Reg32 R5(bus, clk, reset, R5in, R5Val); 
+   Reg32 R6(bus, clk, reset, R6in, R6Val); 
+   Reg32 R7(bus, clk, reset, R7in, R7Val); 
+   Reg32 R8(bus, clk, reset, R8in, R8Val); 
+   Reg32 R9(bus, clk, reset, R9in, R9Val); 
+   Reg32 R10(bus, clk, reset, R10in, R10Val); 
+   Reg32 R11(bus, clk, reset, R11in, R11Val); 
+   Reg32 R12(bus, clk, reset, R12in, R12Val); 
+   Reg32 R13(bus, clk, reset, R13in, R13Val); 
+   Reg32 R14(bus, clk, reset, R14in, R14Val); 
+   Reg32 R15(bus, clk, reset, R15in, R15Val); 
+
+   
 
 
 endmodule 
