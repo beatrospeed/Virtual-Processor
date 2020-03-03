ee6a39b3e9bba735a42c1827326434bf674e7894
beatrospeed <44065356+beatrospeed@users.noreply.github.com>
2020-03-02 21:12:51 -0500

    updated 32to1mux - removed extra inputs

--- Mux_32_to_1.v
+++ Mux_32_to_1.v
@@ -1,4 +1,4 @@
-module Mux_32_to_1(output reg [31:0] BusMuxOut,input [4:0] Select ,input BusMuxin_R0,input BusMuxin_R1,input BusMuxin_R2,input BusMuxin_R3,input BusMuxin_R4,input BusMuxin_R5,input BusMuxin_R6,input BusMuxin_R7,input BusMuxin_R8,input BusMuxin_R9,input BusMuxin_R10,input BusMuxin_R11,input BusMuxin_R12,input BusMuxin_R13,input BusMuxin_R14,input BusMuxin_R15,input BusMuxin_HIout,input BusMuxin_LOout,input BusMuxin_Zhighout,input BusMuxin_Zlowout,input BusMuxin_PCout,input BusMuxin_MDRout,input BusMuxin_In_Portout,input BusMuxin_Cout,input BusMuxin_in24,input BusMuxin_in25,input BusMuxin_in26,input BusMuxin_in27,input BusMuxin_in28,input BusMuxin_in29,input BusMuxin_in30,input BusMuxin_in31);
+module Mux_32_to_1(output reg [31:0] BusMuxOut,input BusMuxin_R0,input BusMuxin_R1,input BusMuxin_R2,input BusMuxin_R3,input BusMuxin_R4,input BusMuxin_R5,input BusMuxin_R6,input BusMuxin_R7,input BusMuxin_R8,input BusMuxin_R9,input BusMuxin_R10,input BusMuxin_R11,input BusMuxin_R12,input BusMuxin_R13,input BusMuxin_R14,input BusMuxin_R15,input BusMuxin_HIout,input BusMuxin_LOout,input BusMuxin_Zhighout,input BusMuxin_Zlowout,input BusMuxin_PCout,input BusMuxin_MDRout,input BusMuxin_In_Portout,input BusMuxin_Cout,input [4:0] Select);
 
 always@(*)
 	begin
@@ -26,14 +26,6 @@ always@(*)
 	else if(Select==21) BusMuxOut =BusMuxin_MDRout;
 	else if(Select==22) BusMuxOut =BusMuxin_In_Portout;
 	else if(Select==23) BusMuxOut =BusMuxin_Cout;
-	else if(Select==24) BusMuxOut =BusMuxin_in24;
-	else if(Select==25) BusMuxOut =BusMuxin_in25;
-	else if(Select==26) BusMuxOut =BusMuxin_in26;
-	else if(Select==27) BusMuxOut =BusMuxin_in27;
-	else if(Select==28) BusMuxOut =BusMuxin_in28;
-	else if(Select==29) BusMuxOut =BusMuxin_in29;
-	else if(Select==30) BusMuxOut =BusMuxin_in30;
-	else if(Select==31) BusMuxOut =BusMuxin_in31;
 	end
 endmodule
 	
